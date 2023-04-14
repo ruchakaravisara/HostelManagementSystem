a41a6dc8682b159fb0f47a83acbd630cbd31a45e
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-04-14 22:13:07 +0530

    update

--- src/lk/ijse/hostel_management_system/bo/BOFactory.java
+++ src/lk/ijse/hostel_management_system/bo/BOFactory.java
@@ -1,4 +1,35 @@
 package lk.ijse.hostel_management_system.bo;
 
+import lk.ijse.hostel_management_system.bo.custom.impl.ReservationBOImpl;
+import lk.ijse.hostel_management_system.bo.custom.impl.RoomBOImpl;
+import lk.ijse.hostel_management_system.bo.custom.impl.StudentBOImpl;
+import lk.ijse.hostel_management_system.bo.custom.impl.UserBOImpl;
+
 public class BOFactory {
+    private static BOFactory BOFactory;
+    public BOFactory(){}
+
+    public static BOFactory getInstance(){
+        if (BOFactory == null){
+            BOFactory = new BOFactory();
+
+        }
+        return BOFactory;
+    }
+    public enum BOTypes{
+        Student,Rooms,Reservation,Users
+    }
+    public static SuperBO getBO(BOTypes boTypes){
+        switch (boTypes){
+            case Student: return (SuperBO) new StudentBOImpl();
+            case Reservation:
+                return (SuperBO) new ReservationBOImpl();
+            case Rooms:
+                return (SuperBO) new RoomBOImpl();
+            case Users:
+                return (SuperBO) new UserBOImpl();
+            default:
+                return null;
+        }
+    }
 }
