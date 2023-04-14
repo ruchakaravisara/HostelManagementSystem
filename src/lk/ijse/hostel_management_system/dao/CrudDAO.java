d19834dd43f75769941120db9dd58772d25912b6
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-04-14 22:32:00 +0530

    dao package update

--- src/lk/ijse/hostel_management_system/dao/CrudDAO.java
+++ src/lk/ijse/hostel_management_system/dao/CrudDAO.java
@@ -1,4 +1,12 @@
 package lk.ijse.hostel_management_system.dao;
 
-public interface CrudDAO {
+import java.util.List;
+
+public interface CrudDAO<T> extends SuperDAO {
+    String save(T obj) throws Exception;
+    void update(T obj) throws Exception;
+    void delete(T obj) throws Exception;
+    T getObject(String id) throws Exception;
+    List<T>loadAll() throws Exception;
+    String generateID() throws Exception;
 }
--- src/lk/ijse/hostel_management_system/dao/DAOFactory.java
+++ src/lk/ijse/hostel_management_system/dao/DAOFactory.java
@@ -1,4 +1,36 @@
 package lk.ijse.hostel_management_system.dao;
 
+import lk.ijse.hostel_management_system.dao.custom.impl.*;
+
 public class DAOFactory {
+     public static DAOFactory daoFactory;
+     public DAOFactory(){}
+
+    public static DAOFactory getDaoFactory(){
+         if (daoFactory == null){
+             daoFactory = new DAOFactory();
+         }
+         return daoFactory;
+    }
+    public enum DAOTypes{
+        Student,Rooms,Reservation,Users,Query
+    }
+
+    public SuperDAO getDAO(DAOTypes daoTypes){
+         switch (daoTypes){
+             case Student :
+                 return (SuperDAO) new StudentDAOImpl();
+             case Rooms:
+                 return (SuperDAO) new RoomDAOImpl();
+             case Reservation:
+                 return (SuperDAO) new ReservationDAOImpl();
+             case Users:
+                 return (SuperDAO) new UserDAOImpl();
+             case Query:
+                 return (SuperDAO) new QueryDAOImpl();
+
+             default:
+                 return null;
+         }
+    }
 }
