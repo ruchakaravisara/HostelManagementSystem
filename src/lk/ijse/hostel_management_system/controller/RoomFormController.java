f40be2b68613b18218d8c81838268c1b8813472e
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-04-18 23:51:35 +0530

    Roomcontroller update

--- src/lk/ijse/hostel_management_system/controller/RoomFormController.java
+++ src/lk/ijse/hostel_management_system/controller/RoomFormController.java
@@ -2,10 +2,19 @@ package lk.ijse.hostel_management_system.controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import lk.ijse.hostel_management_system.bo.BOFactory;
+import lk.ijse.hostel_management_system.bo.custom.RoomBO;
+import lk.ijse.hostel_management_system.dto.RoomDTO;
+
+import java.util.List;
 
 public class RoomFormController {
     public JFXButton btnAddRoom;
@@ -16,21 +25,107 @@ public class RoomFormController {
     public JFXButton btnAdd;
     public JFXButton btnUpdate;
     public JFXButton btnDelete;
-    public TableView tblRoom;
-    public TableColumn colRoomTypeId;
-    public TableColumn colType;
-    public TableColumn colKeyMoey;
-    public TableColumn colQty;
+    public TableView <RoomDTO> tblRoom;
+    public TableColumn<RoomDTO, String> colRoomTypeId;
+    public TableColumn<RoomDTO, String> colType;
+    public TableColumn<RoomDTO, String> colKeyMoey;
+    public TableColumn<RoomDTO, Integer> colQty;
+    public JFXTextField txtRoomId;
+
+    RoomBO roomBO =(RoomBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.Rooms);;
+    public static ObservableList obList = FXCollections.observableArrayList();
+
+    public  void initialize() throws Exception {
+        loadAllRoom();
+        getData();
+        colRoomTypeId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colType.setCellValueFactory(new PropertyValueFactory<>("type"));
+        colKeyMoey.setCellValueFactory(new PropertyValueFactory<>("keyMoney"));
+        colQty.setCellValueFactory(new PropertyValueFactory<>("qty"));
+    }
+
+    private void loadAllRoom() throws Exception {
+        tblRoom.getItems().clear();
+        List<RoomDTO> studentDTOS = roomBO.loadAllRoom();
+        ObservableList<RoomDTO> observableList =FXCollections.observableList(studentDTOS);
+        tblRoom.setItems(observableList);
+    }
+    void clear(){
+        txtRoomId.clear();
+        txtType.clear();;
+        txtKeyMoney.clear();
+        txtQty.clear();
+    }
 
     public void btnAddRoomOnaction(ActionEvent actionEvent) {
     }
 
-    public void btnaddOnAction(ActionEvent actionEvent) {
+    public void btnaddOnAction(ActionEvent actionEvent) throws Exception {
+        boolean isSave =roomBO.saveRoom(new RoomDTO(
+                txtRoomId.getText(),
+                txtType.getText(),
+                txtKeyMoney.getText(),
+                Integer.parseInt(txtQty.getText()
+        )));
+        if (isSave){
+            new Alert(Alert.AlertType.CONFIRMATION, "Room Save Successfully!").show();
+            clear();
+
+            tblRoom.getItems().clear();
+            loadAllRoom();
+        }else {
+            new Alert(Alert.AlertType.WARNING, "Something happened!").show();
+        }
     }
 
-    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    public void btnUpdateOnAction(ActionEvent actionEvent) throws Exception {
+        RoomDTO roomDTO =new RoomDTO(
+               txtRoomId.getText(),
+                txtType.getText(),
+                txtKeyMoney.getText(),
+                Integer.parseInt(txtQty.getText())
+        );
+        boolean isUpdate = roomBO.updateRoom(roomDTO);
+        if (isUpdate){
+            new Alert(Alert.AlertType.CONFIRMATION, "Room Update Successfully!").show();
+            clear();
+
+            tblRoom.getItems().clear();
+            loadAllRoom();
+        }else{
+            new Alert(Alert.AlertType.WARNING, "Something happened!").show();
+        }
     }
 
-    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    public void btnDeleteOnAction(ActionEvent actionEvent) throws Exception {
+        RoomDTO roomDTO =new RoomDTO(
+                txtRoomId.getText(),
+                txtType.getText(),
+                txtKeyMoney.getText(),
+                Integer.parseInt(txtQty.getText())
+        );
+        boolean isDeleted = roomBO.deleteRoom(roomDTO);
+        if (isDeleted){
+            new Alert(Alert.AlertType.CONFIRMATION, "Room Delete Successfully!").show();
+
+            clear();
+            tblRoom.getItems().clear();
+            loadAllRoom();
+        }else{
+
+            new Alert(Alert.AlertType.WARNING, "Something happened!").show();
+        }
+    }
+    void getData(){
+        tblRoom.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+
+
+            if (newValue != null) {
+                txtRoomId.setText(newValue.getId());
+                txtType.setText(newValue.getType());
+                txtQty.setText(String.valueOf(newValue.getQty()));
+                txtKeyMoney.setText(newValue.getKeyMoney());
+            }
+        });
     }
 }
